ada3a84f7e5ed7146f41495ac955dbc6d58162a5
Megan Lo <hkmeganlo@gmail.com>
2020-08-21 21:26:49 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,7 @@
 # Write your code below game_hash
+require 'pry'
+
+
 def game_hash
   {
     home: {
@@ -127,3 +130,100 @@ def game_hash
 end
 
 # Write code here
+def num_points_scored(player_search)
+  game_hash.each do |team, team_info|
+    team_info[:players].each do |player|
+      if player[:player_name] == player_search
+        return player[:points]
+      end
+    end
+  end
+end
+
+def shoe_size(player_search)
+  game_hash.each do |team, team_info|
+    team_info[:players].each do |player|
+      if player[:player_name] == player_search
+        return player[:shoe]
+      end
+    end
+  end
+end
+
+def team_colors(team_name)
+    if team_name.downcase == "charlotte hornets"
+      return game_hash[:away][:colors]
+    else 
+      return game_hash[:home][:colors]
+    end
+end
+
+def team_names
+  game_hash.collect do |team, team_info|
+    team_info[:team_name]
+  end
+end
+
+def player_numbers(team_names)
+  jersey_numbers = []
+  game_hash.each do |team, team_info|
+    if team_info[:team_name] == team_names
+      team_info.each do |key, value|
+        if key == :players
+          value.each do |player|
+            jersey_numbers.push(player[:number])
+          end
+        end
+      end
+    end
+  end
+  jersey_numbers
+end
+
+def player_stats(name)
+  game_hash.each do |team, team_info|
+    team_info.each do |key, value|
+      if key == :players
+        value.each do |player|
+          if name == player[:player_name]
+            return player
+          end
+        end
+      end
+    end
+  end
+end
+
+def big_shoe_rebounds
+  big_shoe = 0
+  rebounds = 0
+  
+  game_hash.each do |team, team_info|
+    team_info[:players].each do |player|
+      if player[:shoe] > big_shoe
+        big_shoe = player[:shoe]
+        rebounds = player[:rebounds]
+      end
+    end
+  end
+  
+  return rebounds
+end
+
+
+#bonus questions
+
+def most_points_scored
+  points = 0
+  
+  game_hash.each do |team, team_info|
+    team_info[:players].each do |player|
+      if player[:points] > points
+        points = player[:points]
+        player[:player_name]
+      end
+    end
+  end
+  
+  
+end
